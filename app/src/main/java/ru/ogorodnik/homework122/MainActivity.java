65c71991aa894ee466f5d5fca1d9ef33c08f550b
OgorodnikSergey <osv8487@gmail.com>
2019-05-10 03:10:00 +0300

    Home Work 122

---
+++ app/src/main/java/ru/ogorodnik/homework122/MainActivity.java
@@ -0,0 +1,43 @@
+package ru.ogorodnik.homework122;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final TextView linkText = (TextView) findViewById(R.id.wwwlink);
+        final Button frontBtn = (Button) findViewById(R.id.buttonFront);
+        final Button backBtn = (Button) findViewById(R.id.buttonBack);
+
+        int num = 1+(int)((100-1+1)*Math.random());
+        linkText.setText("http://myfile.org/" + num);
+
+// Нажатие кнопки ВПЕРЕД ---------------------------------------------------------------------
+        frontBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+// Нажатие кнопки НАЗАД ---------------------------------------------------------------------
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            finish();
+            }
+        });
+
+    }
+}
